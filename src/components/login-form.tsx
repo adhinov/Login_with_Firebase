37e200af5b826e922921a5c1418ed11cc2066c60
adhinov <adhinov4@gmail.com>
2025-09-21 16:54:46 +0000

    more grey again...

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -75,7 +75,7 @@ export function LoginForm() {
       </CardHeader>
       <CardContent>
         <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
             <FormField
               control={form.control}
               name="email"
