95ac5666bde4feec0498bcdaf6104bf4eba0cb7c
adhinov <adhinov4@gmail.com>
2025-09-22 01:29:40 +0000

    ok, now make more top margin for the login button don't closer to foegot

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -165,7 +165,7 @@ export function LoginForm() {
                   <Link href="/forgot-password">Forgot password?</Link>
                 </Button>
             </div>
-            <Button type="submit" className="w-full text-lg py-6 mt-64" disabled={form.formState.isSubmitting}>
+            <Button type="submit" className="w-full text-lg py-6 mt-96" disabled={form.formState.isSubmitting}>
               <LogIn className="mr-2 h-5 w-5" /> Login
             </Button>
           </form>
