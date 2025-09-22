08c4cf3a4d86acba3377a114061cb99b7a288736
adhinov <adhinov4@gmail.com>
2025-09-22 01:35:50 +0000

    now change the line colour in to dark grey

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -142,7 +142,7 @@ export function LoginForm() {
                 </FormItem>
               )}
             />
-            <div className="flex items-center justify-between pt-4">
+            <div className="flex items-center justify-between pt-2">
               <FormField
                 control={form.control}
                 name="rememberMe"
@@ -166,7 +166,7 @@ export function LoginForm() {
                   <Link href="/forgot-password">Forgot password?</Link>
                 </Button>
             </div>
-            <Separator className="my-8" />
+            <Separator className="my-8 bg-gray-600" />
             <Button type="submit" className="w-full text-lg py-6" disabled={form.formState.isSubmitting}>
               <LogIn className="mr-2 h-5 w-5" /> Login
             </Button>
