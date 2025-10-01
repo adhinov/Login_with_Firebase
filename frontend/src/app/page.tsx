5e93404e2016bafa3368a3e7c564cacc8a702922
adhinov <adhinov4@gmail.com>
2025-10-01 21:07:19 +0700

    perubahan page.tsx

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,6 +1,7 @@
-// src/app/page.tsx
+// frontend/src/app/page.tsx
 import { redirect } from "next/navigation";
 
 export default function Home() {
+  // Begitu user akses domain utama (/), langsung redirect ke /login
   redirect("/login");
 }
