c924c68c6061be1cc6becb8c7fa5ae33a7a03a89
adhinov <adhinov4@gmail.com>
2025-10-01 20:27:19 +0700

    penambahan vercel.json 3

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,12 +1,6 @@
 // src/app/page.tsx
-"use client";
+import { redirect } from "next/navigation";
 
-import LoginForm from "@/components/login-form";
-
-export default function LoginPage() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-4 bg-background">
-      <LoginForm />
-    </main>
-  );
+export default function Home() {
+  redirect("/login");
 }
