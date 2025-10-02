93b1aa3bfa34666998f2d6c57e7f424c0659a054
adhinov <adhinov4@gmail.com>
2025-10-02 16:31:35 +0700

    perubahan app/page.tsx

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,3 +1,6 @@
+import { redirect } from "next/navigation";
+
 export default function Home() {
-  return <h1>Hello World from /</h1>;
+  // Begitu user buka "/", langsung redirect ke /login
+  redirect("/login");
 }
