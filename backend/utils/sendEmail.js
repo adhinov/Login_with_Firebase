8ba349b3761fabd4b8f45e9da33f05d51961d792
adhinov <adhinov4@gmail.com>
2025-10-07 15:05:54 +0700

    perbaikan sendEmail.js

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,18 +1,22 @@
-// utils/sendEmail.js
 import { Resend } from "resend";
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const sendEmail = async (to, subject, html) => {
   try {
-    await resend.emails.send({
+    const response = await resend.emails.send({
       from: process.env.FROM_EMAIL,
       to,
       subject,
       html,
     });
-    console.log(`✅ Email terkirim ke ${to}`);
+
+    console.log("✅ Email response:", response);
   } catch (error) {
-    console.error("❌ Gagal mengirim email:", error);
+    console.error("❌ Gagal mengirim email:");
+    if (error.response) {
+      console.error("Response:", error.response);
+    }
+    console.error("Message:", error.message);
     throw new Error("Gagal mengirim email");
   }
 };
