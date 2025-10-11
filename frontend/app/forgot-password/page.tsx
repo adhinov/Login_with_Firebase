48a1c3b80ceb3c0087f5805b32243e9f8494989c
adhinov <adhinov4@gmail.com>
2025-10-11 13:07:43 +0700

    perubahan forgot-password-form.tsx 4

--- frontend/app/forgot-password/page.tsx
+++ frontend/app/forgot-password/page.tsx
@@ -1,12 +1,11 @@
 "use client";
 
-import { Suspense } from "react";
-import ForgotPasswordContent from "../../src/components/forgot-password-form";
+import ForgotPasswordForm from "@/components/forgot-password-form";
 
 export default function ForgotPasswordPage() {
   return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <ForgotPasswordContent />
-    </Suspense>
+    <div className="min-h-screen flex items-center justify-center bg-background">
+      <ForgotPasswordForm />
+    </div>
   );
 }
