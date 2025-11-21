8fcbfcd92fcfb71586077997b8f4a538566dac6a
adhinov <adhinov4@gmail.com>
2025-11-21 15:27:48 +0700

    perubahan cloudinary

--- backend/config/cloudinary.js
+++ backend/config/cloudinary.js
@@ -6,4 +6,10 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
+console.log("Cloudinary config:", {
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY ? "OK" : "MISSING",
+  api_secret: process.env.CLOUDINARY_API_SECRET ? "OK" : "MISSING"
+});
+
 export default cloudinary;
